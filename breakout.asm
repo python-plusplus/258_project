7eff0c4fd4d6acc12dabc1483608bc1c09ad2d94
python-plusplus <sunghyoun.kim@mail.utoronto.ca>
2023-03-28 23:37:27 -0400

    milestone 1

--- breakout.asm
+++ breakout.asm
@@ -4,10 +4,10 @@
 # Student 1: Name, 1008421931
 # Student 2: Name, Student Number
 ######################## Bitmap Display Configuration ########################
-# - Unit width in pixels:       TODO
-# - Unit height in pixels:      TODO
-# - Display width in pixels:    TODO
-# - Display height in pixels:   TODO
+# - Unit width in pixels:       8
+# - Unit height in pixels:      8
+# - Display width in pixels:   	512
+# - Display height in pixels:   512
 # - Base Address for Display:   0x10008000 ($gp)
 ##############################################################################
 
@@ -25,6 +25,8 @@ ADDR_KBRD:
 #Strings to print using syscall if game starts or ends
 START_STR: .asciiz "GAME STARTED"
 QUIT_STR: .asciiz "GAME ENDED"
+
+
 
 ##############################################################################
 # Mutable Data
@@ -54,19 +56,62 @@ handle_menu_options:
 	beq $a0, 0x71, quit_game #if q is pressed quit
     beq $a0, 0x20, start_game  #if space is pressed start
     b main
-   
-start_game:
-	li $v0, 4
-	la $a0, START_STR
-	syscall
-	b game_loop
-	
-quit_game:
+  
+ quit_game:
 	li $v0, 4 #Set syscall to print string
 	la $a0, QUIT_STR
 	syscall
 	li $v0, 10
 	syscall
+	
+start_game:
+	li $v0, 4
+	la $a0, START_STR
+	syscall
+	#Initialize game environment
+	
+	lw $t4, ADDR_DSPL
+	
+	#Initialize amount of bricks 256 * 6 rows
+	addi $t5, $t4, 3072
+	#Add margin top: 2 rows = 256 * 2 = 512
+	addi $t4, $t4, 512
+	li $t1, 0xff0000
+	b draw_bricks
+	
+draw_bricks:
+	jal draw_brick
+	addi $t4, $t4, 4
+	beq $t4, $t5, draw_paddle
+	b draw_bricks
+	
+	
+draw_brick:
+	sw $t1, 0($t4)
+	jr $ra
+
+draw_paddle:
+	#Set colour to blue
+	li $t1, 0x0000ff
+	#Initialize paddle 12 rows down from the last brick, and center 256/2 = 128, 6144 + 128 = 6272
+	addi $t4, $t4, 6272
+	sw $t1, -16($t4)
+	sw $t1, -12($t4)
+	sw $t1, -8($t4)
+	sw $t1, -4($t4)
+	sw $t1, 0($t4)
+	sw $t1, 4($t4)
+	sw $t1, 8($t4)
+	sw $t1, 12($t4)
+	sw $t1, 16($t4)
+	b draw_ball
+	
+draw_ball:
+	li $t2, 0x00ff00 #$t2 is green
+	#Initialize $t3 as ball location
+	addi $t3, $t4, -256
+	sw $t2, 0($t3)
+	b game_loop
 	
 game_loop:
 	# 1a. Check if key has been pressed
@@ -78,10 +123,3 @@ game_loop:
 
     #5. Go back to 1
     b game_loop
-
-draw_bricks:
-	#Draw blocks onto bitmap display at initialization step.
-	
-
-draw_brick:
-	#Draw one brick
